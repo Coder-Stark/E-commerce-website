21873d7369760a562b56ff8839e45f6fac451d5a
Coder-Stark <shivamkumar789321@gmail.com>
2023-05-26 23:30:39 +0530

    adding_encryption fixed error

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,21 +1,27 @@
 import User from "@/models/User"
 import connectDb from "@/middleware/mongoose"
+var CryptoJS = require("crypto-js");
 
 const handler = async (req,res)=>{
   if(req.method == 'POST'){
     console.log(req.body);
     let user = await User.findOne({"email": req.body.email})
+    const bytes = CryptoJS.AES.decrypt(user.password, "secret123");
+    let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
     if(user){
-        if(req.body.email == user.email && req.body.password == user.password){
+        if(req.body.email == user.email && req.body.password == decryptedPass){
             res.status(200).json({success: "true", email: user.email, name:user.name})
         }
         else{
             res.status(200).json({success: "false", error: "Invalid Credentials"})
         }
     }
+    else{
+      res.status(200).json({error: 'No user Found'})
+    }
   }
   else{
-    res.status(200).json({error: 'No user Found'})
+    res.status(400).json({error: "This method is not allowed"})
   }
 }
 export default connectDb(handler);
