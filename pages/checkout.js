5eecf053bc336d07d9caf76c91c2c76cc4b2ae2a
Coder-Stark <shivamkumar789321@gmail.com>
2023-11-14 19:42:18 +0530

    checkoutpage update

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,184 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Link from 'next/link';
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
 import {MdShoppingCartCheckout} from 'react-icons/md'
-import Head from 'next/head';
-import Script from 'next/script';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-const Checkout = ({cart, clearCart, subTotal, addToCart, removeFromCart }) => {
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [address,setAddress] = useState('');
-  const [phone,setPhone] = useState('');
-  const [pincode,setPincode] = useState('');
-  const [state,setState] = useState('');
-  const [city,setCity] = useState('');
-  const [disabled,setDisabled] = useState(true);
-  const [user, setUser] = useState({value: null});
-
-  useEffect(()=>{
-    const user = JSON.parse(localStorage.getItem('myuser'));
-    if(user.token){
-      setUser(user);
-      setEmail(user.email);
-    }
-  },[])
-
-  const handleChange = async(e)=>{
-    console.log(user,email);
-
-    if(e.target.name == 'name'){
-      setName(e.target.value);
-    } 
-    else if(e.target.name == 'email'){
-      setEmail(e.target.value);
-    }
-    else if(e.target.name == 'address'){
-      setAddress(e.target.value);
-    }
-    else if(e.target.name == 'phone'){
-      setPhone(e.target.value);
-    }
-    else if(e.target.name == 'pincode'){
-      setPincode(e.target.value);
-      if(e.target.value == 6){
-        let pins = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pincode`)
-        let pinJson = await pins.json();
-        if(Object.keys(pinJson).includes(e.target.value)){
-          setState(pinJson[e.target.value][0]);
-          setCity(pinJson[e.target.value][1]);
-        }
-        else{
-          setState('');
-          setCity('');
-        }
-      }
-      else{
-        setState('');
-        setCity('');
-      }
-    }
-    setTimeout(()=>{
-      if(name.length > 3 && email.length > 3 && phone.length > 3 && address.length > 3 && pincode.length > 3){
-        setDisabled(false);
-      }
-      else{
-        setDisabled(true);
-      }
-    },100)
-  }
-
-  const initiatePayment = async()=>{
-    let oid = Math.floor(Math.random() * Date.now());
-    //Get a transaction token
-    const data = { cart, subTotal , oid, email : email, name, address, pincode, phone};
-
-    let a = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pretransaction`,{
-      method: "POST", // or 'PUT'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-    let txnRes = await a.json();
-    if(txnRes.success){
-    // console.log(txnRes); 
-    let txnToken = txnRes.txnToken;
-      
-    var config = {
-      "root": "",
-      "flow": "DEFAULT",
-      "data": {
-      "orderId": oid , /* update order id */
-      "token": txnToken, /* update token value */
-      "tokenType": "TXN_TOKEN",
-      "amount": subTotal /* update amount */
-      },
-      "handler": {
-        "notifyMerchant": function(eventName,data){
-          console.log("notifyMerchant handler function called");
-          console.log("eventName => ",eventName);
-          console.log("data => ",data);
-        } 
-      }
-    };
-
-    window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
-        // after successfully updating configuration, invoke JS Checkout
-        window.Paytm.CheckoutJS.invoke();
-    }).catch(function onError(error){
-        console.log("error => ",error);
-    });
-    }
-    else{
-      console.log(txnRes.error);
-      clearCart();
-      toast.error(txnRes.error, {
-        position: "top-left",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-  }
+const Checkout = ({cart, subTotal, addToCart, removeFromCart }) => {
+  const handleCashOnDelivery = () => {
+    // You can add logic here to handle the Cash on Delivery process
+    // For example, you can show a success message and clear the cart.
+    console.log('Cash on Delivery chosen. Implement your logic here.');
+  };
   return (
     <div className='container px-2 m-auto'>
-      <ToastContainer
-      position="top-left"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-      />
-
-      <Head><meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"/></Head>
-      <Script type="application/javascript" crossorigin="anonymous" src={`${process.env.NEXT_PUBLIC_PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.NEXT_PUBLIC_PAYTM_MID}.js`}/>
-
       <h1 className='font-bold text-3xl my-8 text-center'>Checkout</h1>
       <h2 className='semi-bold text-xl'>1. Delivery Details</h2>
       <div className="mx-auto flex">
         <div className="px-2 w-1/2">
           <div className=" mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-            <input onChange={handleChange} value={name} type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className=" mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-            {user && user.value?<input value={user.email} type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" readOnly/> :<input onChange={handleChange} value={email} type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/> }
-            
+            <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
       </div>
         <div className="px-2 w-full">
           <div className=" mb-4">
             <label htmlFor="address" className="leading-7 text-sm text-gray-600">Address</label>
-            <textarea onChange={handleChange} value={address}  name="address" id="address" cols="30" rows="2" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
+            <textarea name="address" id="address" cols="30" rows="2" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
           </div>
         </div>
       <div className="mx-auto flex">
         <div className="px-2 w-1/2">
           <div className=" mb-4">
             <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone</label>
-            <input  onChange={handleChange} value={phone} type="phone" id="phone" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <input type="phone" id="phone" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className=" mb-4">
-            <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">PinCode</label>
-            <input onChange={handleChange} value={pincode} type="text" id="pincode" name="pincode" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <label htmlFor="city" className="leading-7 text-sm text-gray-600">City</label>
+            <input type="text" id="city" name="city" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
       </div>
@@ -186,13 +51,13 @@ const Checkout = ({cart, clearCart, subTotal, addToCart, removeFromCart }) => {
         <div className="px-2 w-1/2">
           <div className=" mb-4">
             <label htmlFor="state" className="leading-7 text-sm text-gray-600">State</label>
-            <input onChange={handleChange} value={state} type="text" id="state" name="state" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <input type="text" id="state" name="state" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className=" mb-4">
-            <label htmlFor="city" className="leading-7 text-sm text-gray-600">City</label>
-            <input onChange={handleChange} value={city} type="text" id="city" name="city" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">PinCode</label>
+            <input type="text" id="pincode" name="pincode" className="w-full bg-white rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
       </div>
@@ -211,7 +76,7 @@ const Checkout = ({cart, clearCart, subTotal, addToCart, removeFromCart }) => {
         <span className='font-bold'>Subtotal: ₹{subTotal}</span>
       </div>
       <div className="mx-4">
-          <Link href={'/checkout'}><button disabled={disabled}  onClick={initiatePayment} className="disabled:bg-gray-300 flex mr-6 px-1 text-white bg-gray-500 border-0  focus:outline-none hover:bg-gray-600 rounded text-md  items-center"><MdShoppingCartCheckout className='m-1'/>Pay ₹{subTotal}</button></Link>
+          <Link href={'/checkout'}><button  onClick={handleCashOnDelivery} className="flex mr-6 px-1 text-white bg-gray-500 border-0  focus:outline-none hover:bg-gray-600 rounded text-md  items-center"><MdShoppingCartCheckout className='m-1'/>Pay ₹{subTotal}</button></Link>
       </div>
     </div>
   )
